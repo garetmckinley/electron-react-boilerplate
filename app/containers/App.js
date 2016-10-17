4e4de41d743b63ccd98e521599c53f4cd91e9de5
Garet McKinley <self@mediachicken.com>
2016-10-17 19:27:03 -0400

    integrated styled-components

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,5 +1,19 @@
 // @flow
+/* eslint-disable no-unused-expressions */
 import React, { Component, PropTypes } from 'react';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    position: relative;
+    color: white;
+    height: 100vh;
+    background-color: #232C39;
+    background-image: linear-gradient(45deg, rgba(0, 216, 255, .5) 10%, rgba(0, 1, 127, .7));
+    font-family: Arial, Helvetica, Helvetica Neue, serif;
+    overflow-y: hidden;
+  }
+`;
 
 export default class App extends Component {
   static propTypes = {
