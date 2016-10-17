64b66844dc757272829541146db46b9cf7ad0358
Garet McKinley <self@mediachicken.com>
2016-10-17 19:26:24 -0400

    integrated styled components

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,7 +1,32 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
-import styles from './Counter.css';
+import styled from 'styled-components';
+import A from './Common/A';
+import Button from './Common/Button';
+
+const BackButton = styled.div`
+  position: absolute;
+`;
+
+const ButtonGroup = styled.div`
+  margin: 0 auto;
+  position: absolute;
+  text-align: center;
+  bottom: 20px;
+  width: 100%;
+`;
+
+const Container = styled.div`
+`;
+
+const CounterLabel = styled.div`
+  font-size: 10rem;
+  font-weight: bold;
+  left: 45%;
+  letter-spacing: -.025em;
+  position: absolute;
+  top: 30%;
+`;
 
 class Counter extends Component {
   static propTypes = {
@@ -15,26 +40,26 @@ class Counter extends Component {
   render() {
     const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
     return (
-      <div>
-        <div className={styles.backButton}>
-          <Link to="/">
+      <Container>
+        <BackButton>
+          <A to="/">
             <i className="fa fa-arrow-left fa-3x" />
-          </Link>
-        </div>
-        <div className={`counter ${styles.counter}`}>
+          </A>
+        </BackButton>
+        <CounterLabel>
           {counter}
-        </div>
-        <div className={styles.btnGroup}>
-          <button className={styles.btn} onClick={increment}>
+        </CounterLabel>
+        <ButtonGroup>
+          <Button onClick={increment}>
             <i className="fa fa-plus" />
-          </button>
-          <button className={styles.btn} onClick={decrement}>
+          </Button>
+          <Button onClick={decrement}>
             <i className="fa fa-minus" />
-          </button>
-          <button className={styles.btn} onClick={incrementIfOdd}>odd</button>
-          <button className={styles.btn} onClick={() => incrementAsync()}>async</button>
-        </div>
-      </div>
+          </Button>
+          <Button onClick={incrementIfOdd}>odd</Button>
+          <Button onClick={() => incrementAsync()}>async</Button>
+        </ButtonGroup>
+      </Container>
     );
   }
 }
