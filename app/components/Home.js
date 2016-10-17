e6f648a26f4e3df7a6936c007584bcb9396d649c
Garet McKinley <self@mediachicken.com>
2016-10-17 16:50:31 -0400

    moved styled from Home.css into Home.js (styled-components)

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,18 +1,34 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from './Home.css';
+import styled from 'styled-components';
 
+const Container = styled.section`
+  position: absolute;
+  top: 30%;
+  left: 10px;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  font-size: 5rem;
+  font-weight: bold;
+  letter-spacing: -.025em;
+  color: #fff;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1.4rem;
+`;
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
-      </div>
+      <Container>
+        <Title>Home</Title>
+        <StyledLink to="/counter">to Counter</StyledLink>
+      </Container>
     );
   }
 }
