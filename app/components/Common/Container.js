a7e19b5202e44925ede1b08e5546ce8bebf95b52
Garet McKinley <self@mediachicken.com>
2016-10-17 19:25:07 -0400

    added component for styled containers

---
+++ app/components/Common/Container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.section`
+  position: absolute;
+  top: 30%;
+  left: 10px;
+  text-align: center;
+`;
+
+export default Container;
