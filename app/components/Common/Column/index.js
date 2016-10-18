d61000c6f7845bc2c498cae27ab10d32f6a559d3
Garet McKinley <self@mediachicken.com>
2016-10-18 01:46:00 -0400

    added a flexbox Column

---
+++ app/components/Common/Column/index.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+
+function Column({ ...props }) {
+  const StyledColumn = styled.section`
+    -webkit-box-flex: 1;
+    -webkit-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    padding: 1em 0 0 1em;
+  `;
+  return (
+    <StyledColumn>{props.children}</StyledColumn>
+  );
+}
+
+Column.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
+  fontSize: PropTypes.number
+};
+
+export default Column;
